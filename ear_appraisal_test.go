2a5ee3726cb0267c16a11fef73628ec790ffedb7
Jeff Schymiczek <jeff.schymiczek@helsinki.fi>
2026-09-16 11:06:46 +0200

    fix: Added status validation to ear_appraisal
    Partially fixes veraison/services#430. This commit adds functionality
    to ear_appraisal.Validate() to check that the appraisal conforms to
    draft-fv-rats-ear-04. Thus, it is checked that:
     - status is one of the four defined trust tiers
     - status is larger than or equal to each element of the trust vector
    (= of no higher trustworthiness than the trust vector)
    
    Additionally this commit:
     - adds minimal unit tests to Validate() and UpdateStatusFromTrustVector()
     - adds not-nil check for trust vector to UpdateStatusFromTrustVector()
     - exports ear_appraisal.Validate(), such that it could be called when policies
    are validated
    
    Co-authored-by: setrofim <setrofim@proton.me>
    
    Signed-off-by: Jeff Schymiczek <jeff.schymiczek@helsinki.fi>

--- ear_appraisal_test.go
+++ ear_appraisal_test.go
@@ -101,3 +101,95 @@ func TestNewAppraisal(t *testing.T) {
 		assert.Equal(t, NoClaim, claim)
 	}
 }
+
+func TestAppraisal_UpdateStatusFromTrustVector_new(t *testing.T) {
+	appraisal := NewAppraisal()
+
+	appraisal.UpdateStatusFromTrustVector()
+
+	assert.Equal(t, TrustTier(0), *appraisal.Status)
+}
+
+func TestAppraisal_UpdateStatusFromTrustVector_basic(t *testing.T) {
+	appraisal := NewAppraisal()
+	trustVector := TrustVector{
+		InstanceIdentity: -2,
+		Configuration:    54,
+	}
+	appraisal.TrustVector = &trustVector
+
+	appraisal.UpdateStatusFromTrustVector()
+
+	assert.Equal(t, TrustTierWarning, *appraisal.Status)
+}
+
+func TestAppraisal_UpdateStatusFromTrustVector_respects_negative(t *testing.T) {
+	appraisal := NewAppraisal()
+	trustVector := TrustVector{
+		InstanceIdentity: -2,
+		Configuration:    5,
+		FileSystem:       54,
+		Hardware:         -97,
+	}
+	appraisal.TrustVector = &trustVector
+
+	appraisal.UpdateStatusFromTrustVector()
+
+	assert.Equal(t, TrustTierContraindicated, *appraisal.Status)
+}
+
+func TestAppraisal_Validate_nil(t *testing.T) {
+	appraisal := Appraisal{}
+	assert.Error(t, appraisal.Validate(), "missing mandatory 'ear.status'")
+}
+
+func TestAppraisal_Validate_new(t *testing.T) {
+	appraisal := NewAppraisal()
+	assert.Nil(t, appraisal.Validate())
+}
+
+func TestAppraisal_Validate_basic(t *testing.T) {
+	appraisal := NewAppraisal()
+	trustVector := TrustVector{
+		InstanceIdentity: -2,
+		Configuration:    5,
+		Hardware:         32,
+	}
+	appraisal.TrustVector = &trustVector
+	assert.Nil(t, appraisal.Validate())
+
+	testTrustTierAffirming := TrustTierAffirming
+	appraisal.Status = &testTrustTierAffirming
+	assert.Error(t, appraisal.Validate(), "status 2 has higher trustworthiness than Hardware trust vector element(32)")
+
+	testTrustTierWarning := TrustTierWarning
+	appraisal.Status = &testTrustTierWarning
+	assert.Nil(t, appraisal.Validate())
+
+	testTrustTierError := TrustTier(33)
+	appraisal.Status = &testTrustTierError
+	assert.Error(t, appraisal.Validate(), "'ear.status' must be a valid trust tier, but is: 33")
+}
+
+func TestAppraisal_Validate_respects_negative(t *testing.T) {
+	appraisal := NewAppraisal()
+	trustVector := TrustVector{
+		InstanceIdentity: -2,
+		Configuration:    1,
+		Hardware:         -54,
+	}
+	appraisal.TrustVector = &trustVector
+	assert.Nil(t, appraisal.Validate())
+
+	testTrustTierAffirming := TrustTierAffirming
+	appraisal.Status = &testTrustTierAffirming
+	assert.Error(t, appraisal.Validate(), "status 2 has higher trustworthiness than Hardware trust vector element(32)")
+
+	testTrustTierWarning := TrustTierWarning
+	appraisal.Status = &testTrustTierWarning
+	assert.Nil(t, appraisal.Validate())
+
+	testTrustTierContraindicated := TrustTierContraindicated
+	appraisal.Status = &testTrustTierContraindicated
+	assert.Nil(t, appraisal.Validate())
+}
